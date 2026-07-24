b400eda2254fa1f78fee702781f46df01ae5469a
Priyanshu Roshan <priyanshu@leaddesk.com>
2026-07-24 20:26:36 +0530

    chore: Add Vercel debug endpoint to diagnose backend failure

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 const app = require('../server/index.js');
 const { parse } = require('url');
+const mongoose = require('mongoose');
 
 module.exports = (req, res) => {
+  // Debug endpoint to check Vercel environment
+  if (req.url.includes('/api/_debug')) {
+    return res.json({
+      env_mongo: !!process.env.MONGO_URI,
+      env_jwt: !!process.env.JWT_SECRET,
+      db_state: mongoose.connection.readyState,
+      url: req.url,
+      originalUrl: req.headers['x-invoke-path'] || req.url,
+      method: req.method
+    });
+  }
+
   // Parse the query string manually since Vercel might not populate req.query for raw HTTP handlers
   const parsedUrl = parse(req.url, true);
   
